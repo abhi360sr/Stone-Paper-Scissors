2b11410e7de3c1c429bc12b0a3e4ce360ba1925e
ABHISHEK SR <122715891+abhi360sr@users.noreply.github.com>
2024-06-08 17:12:12 +0530

    main.c

--- main.c
+++ main.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Function to implement the game logic
+
 int game(char you, char computer) {
-    // If both the user and computer have chosen the same thing
+   
     if (you == computer)
         return -1;
 
-    // Winning conditions for the user
+
     if ((you == 's' && computer == 'z') || 
         (you == 'p' && computer == 's') || 
         (you == 'z' && computer == 'p'))
@@ -17,32 +17,32 @@ int game(char you, char computer) {
         return 0;
 }
 
-// Driver Code
+
 int main() {
     int n;
     char you, computer;
     int result;
 
-    // Seed the random number generator
+
     srand(time(NULL));
 
-    // Generate a random number between 0 and 99
+  
     n = rand() % 100;
 
-    // Using simple probability, roughly divide 100 among stone, paper, and scissor
+   
     if (n < 33)
-        computer = 's';  // s denotes Stone
+        computer = 's'; 
     else if (n < 66)
-        computer = 'p';  // p denotes Paper
+        computer = 'p';  
     else
-        computer = 'z';  // z denotes Scissor
+        computer = 'z'; 
 
     printf("\n\n\n\n\t\t\t\tEnter s for STONE, p for PAPER and z for SCISSOR\n\t\t\t\t\t\t\t");
 
-    // Input from the user
+
     scanf(" %c", &you);
 
-    // Function call to play the game
+  
     result = game(you, computer);
 
     if (result == -1) {
